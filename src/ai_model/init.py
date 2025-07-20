691c4199a905c201129566b695c4767f4c956eee
sondre <voldensondre@gmail.com>
2025-07-20 18:04:23 +0200

    Create init.py

---
+++ src/ai_model/init.py
@@ -0,0 +1,8 @@
+"""
+AI Model package for FionaSparx
+"""
+
+from .advanced_image_generator import AdvancedImageGenerator
+from .smart_text_generator import SmartTextGenerator
+
+__all__ = ['AdvancedImageGenerator', 'SmartTextGenerator']
